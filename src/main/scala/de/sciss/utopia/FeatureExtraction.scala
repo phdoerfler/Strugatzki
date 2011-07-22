b75a4b78d7f726f780850991f954b2a8c3728155
Sciss <contact@sciss.de>
2011-07-22 20:09:33 +0100

    nrt almost working

--- src/main/scala/de/sciss/utopia/FeatureExtraction.scala
+++ src/main/scala/de/sciss/utopia/FeatureExtraction.scala
@@ -29,15 +29,21 @@
 package de.sciss.utopia
 
 import sys.error
-import java.io.File
-import actors.{DaemonActor, Future}
-import de.sciss.synth.{SynthDef, Server, RevocableFuture}
 import de.sciss.synth
-import synth.io.AudioFile
-import de.sciss.osc.OSCBundle
+import de.sciss.osc.{OSCPacketCodec, OSCBundle}
+import java.io.{RandomAccessFile, File}
+import java.nio.ByteBuffer
+import synth.io.{AudioFileSpec, AudioFileType, SampleFormat, AudioFile}
+import sys.process.{ProcessLogger, Process}
+import xml.XML
+import actors.Actor
 
 object FeatureExtraction {
-   def apply( settings: Settings )( observer: PartialFunction[ ProgressOrResult ]) : FeatureExtraction = {
+   var verbose = true
+
+   private lazy val tmpDir = new File( sys.props.getOrElse( "java.io.tmpdir", "/tmp" ))
+
+   def apply( settings: Settings )( observer: PartialFunction[ ProgressOrResult, Unit ]) : FeatureExtraction = {
       new FeatureExtraction( settings, observer )
    }
 
@@ -50,18 +56,21 @@ object FeatureExtraction {
       def fftOverlap : Int
    }
 
-   object SettingsBuilder { def apply = new SettingsBuilder }
+//   object SettingsBuilder { def apply() = new SettingsBuilder }
    final class SettingsBuilder extends SettingsLike {
       var audioInput : File         = new File( "input.aif" )
-      var featureOutput : File      = new File( sys.props.getOrElse( "java.io.tmp", "/tmp" ), "features.aif" )
+      var featureOutput : File      = new File( tmpDir, "features.aif" )
       var metaOutput                = Option.empty[ File ]
       var numCoeffs : Int           = 13
       var fftSize : Int             = 1024
       var fftOverlap : Int          = 2
 
-      def toSettings = Settings( audioInput, featureOutput, metaOutput, numCoeffs, fftSize, fftOverlap )
+      def build = Settings( audioInput, featureOutput, metaOutput, numCoeffs, fftSize, fftOverlap )
    }
 
+   object Settings {
+      implicit def fromBuilder( sb: SettingsBuilder ) : Settings = sb.build
+   }
    final case class Settings( audioInput : File, featureOutput : File, metaOutput : Option[ File ],
                               numCoeffs : Int, fftSize : Int, fftOverlap : Int )
    extends SettingsLike
@@ -74,108 +83,194 @@ object FeatureExtraction {
    case object Aborted extends Result
 }
 final class FeatureExtraction private ( val settings: FeatureExtraction.Settings,
-                                        observer: PartialFunction[ FeatureExtraction.ProgressOrResult ]) {
+                                        observer: PartialFunction[ FeatureExtraction.ProgressOrResult, Unit ]) {
    import FeatureExtraction._
+
+   Act.start()
 
    def abort() { Act ! Abort }
 
    private object Abort
 
    private object ProcT extends Thread {
+      var aborted: Boolean = false
+      var p: Process = null
+
       override def run() {
-         try {
-            procBody()
-         } finally {
+         Act ! (try {
+            if( procBody() ) Success else Aborted
+         } catch {
+            case e => Failure( e )
+         })
+      }
 
+      private def procBody() : Boolean = {
+         import synth._
+         import ugen._
+
+         val spec             = AudioFile.readSpec( settings.audioInput )
+         val stepSize         = settings.fftSize / settings.fftOverlap
+         val coeffRate        = spec.sampleRate / stepSize
+
+         val so               = new ServerOptionsBuilder
+         val oscF             = File.createTempFile( "tmp", ".osc" )
+         val dummyOutput      = File.createTempFile( "tmp", ".aif" )
+         so.inputBusChannels  = spec.numChannels
+         so.sampleRate        = spec.sampleRate.toInt // coeffRate.toInt
+         so.outputBusChannels = 1
+         so.nrtCommandPath    = oscF.getAbsolutePath
+         so.nrtInputPath      = Some( settings.audioInput.getAbsolutePath )
+         so.nrtOutputPath     = dummyOutput.getAbsolutePath
+         so.nrtHeaderFormat   = AudioFileType.AIFF
+         so.nrtSampleFormat   = SampleFormat.Int16
+
+         val s                = Server.dummy( "nrt", so.build )
+         val coeffBufSize     = 1024
+         val fftBufID         = 0
+         val coeffBufID       = 1
+         val df = SynthDef( "nrt" ) {
+            val in         = Mix( In.ar( NumOutputBuses.ir, spec.numChannels )) // XXX mono mix could be configurable
+            val chain      = FFT( fftBufID, in, 1.0 / settings.fftOverlap )
+            val coeffs     = MFCC.kr( chain, settings.numCoeffs )
+//            val rmax       = RunningMax.kr( coeffs, 0 )
+//            val rmin       = RunningMin.kr( coeffs, 0 )
+            val trig       = Impulse.kr( coeffRate ) - Impulse.kr( 0 )
+            val phaseHi    = coeffBufSize - 1
+//            val phase      = Stepper.kr( trig, lo = 0, hi = phaseHi, resetVal = phaseHi )
+            val phase      = Stepper.kr( trig, 0, 0, phaseHi, phaseHi )
+            BufWr.kr( coeffs, coeffBufID, phase, loop = 0 )
          }
+
+         val syn        = Synth( s )
+         val fftBuf     = new Buffer( s, fftBufID )
+         val coeffBuf   = new Buffer( s, coeffBufID )
+         val numFFTs    = ((spec.numFrames + stepSize - 1) / stepSize).toInt
+         val numWrites  = (numFFTs + coeffBufSize - 1) / coeffBufSize
+
+         def tmpName( i: Int ) = new File( tmpDir, "feat" + i + ".aif" )
+
+         val bufBndls = IndexedSeq.tabulate( numWrites ) { i =>
+            val startFrame = i * coeffBufSize
+            val stopFrame  = math.max( numFFTs, startFrame + coeffBufSize )
+            val numFrames  = stopFrame - startFrame
+            val msg        = coeffBuf.writeMsg( tmpName( i ).getAbsolutePath,
+               AudioFileType.AIFF, SampleFormat.Float,
+               numFrames, 0, false )
+            val time       = (i + 1.5) * stepSize / spec.sampleRate
+            OSCBundle.secs( time, msg )
+         }
+
+         val initBndl = OSCBundle.secs( 0.0,
+            fftBuf.allocMsg( settings.fftSize ),
+            coeffBuf.allocMsg( coeffBufSize, settings.numCoeffs ),
+            df.recvMsg,
+            syn.newMsg( df.name, s.rootNode )
+         )
+
+         val bndls   = initBndl +: bufBndls  // don't bother about n_free and b_free
+
+         val c    = OSCPacketCodec.default
+         val sz   = bndls.map( _.getEncodedSize( c )).max
+         val raf  = new RandomAccessFile( oscF, "rw" )
+         val bb   = ByteBuffer.allocate( sz )
+         val fch  = raf.getChannel
+         bndls.foreach { bndl =>
+            bndl.encode( c, bb )
+            bb.flip
+            raf.writeInt( bb.limit )
+            fch.write( bb )
+            bb.clear
+         }
+         raf.close()
+
+         val dur = OSCBundle.timetagToSecs( bufBndls.last.timetag )
+
+         val log = new ProcessLogger {
+            var lastProg = -1
+            def buffer[ T ]( f: => T ) : T = f  // ???
+            def err( line: => String ) {
+               if( line.startsWith( "nextOSCPacket" )) {
+                  val time = line.substring( 14 ).toFloat
+                  val prog = (time / dur * 100).toInt
+                  if( prog != lastProg ) {
+                     Act ! Progress( prog * 0.008f )  // up to 80%
+                     lastProg = prog
+                  }
+               } else {
+                  Console.out.println( s )
+               }
+            }
+            def out( line: => String ) {
+               Console.err.println( line )
+            }
+         }
+
+         def shouldAbort : Boolean = this.synchronized { aborted }
+
+         val proc = this.synchronized {
+            if( shouldAbort ) return false
+            val args = so.toNonRealtimeArgs
+            if( verbose ) println( args.mkString( "cmd: ", " ", "" ))
+            val pb = Process( args, Some( new File( so.programPath ).getParentFile ))
+            p = pb.run( log )
+            p
+         }
+         val res = proc.exitValue() // blocks
+         this.synchronized {
+            p = null
+            if( shouldAbort ) return false
+         }
+         if( res != 0 ) throw new RuntimeException( "scsynth failed with exit code " + res )
+
+         val afOutS     = AudioFileSpec( AudioFileType.AIFF, SampleFormat.Float, settings.numCoeffs, coeffRate )
+         val afOut      = AudioFile.openWrite( settings.featureOutput, afOutS )
+         for( i <- 0 until numWrites ) {
+            if( shouldAbort ) return false
+            val afIn    = AudioFile.openRead( tmpName( i ))
+            val b       = afIn.frameBuffer( afIn.numFrames.toInt )
+            afIn.readFrames( b )
+            afIn.close
+            afOut.writeFrames( b )
+            afIn.file.foreach( _.delete() )
+            val prog = (i + 1).toFloat / numWrites * 0.2f + 0.8f
+            Act ! Progress( prog )
+         }
+         afOut.close
+
+         settings.metaOutput.foreach { metaFile =>
+            val xml =
+<feature>
+   <input>{settings.audioInput.getAbsolutePath}</input>
+   <output>{settings.featureOutput.getAbsolutePath}</output>
+   <numCoeffs>{settings.numCoeffs}</numCoeffs>
+   <fftSize>{settings.fftSize}</fftSize>
+   <fftOverlap>{settings.fftOverlap}</fftOverlap>
+</feature>
+
+            XML.save( metaFile.getAbsolutePath, xml, "UTF-8", true, null )
+         }
+
+         true
       }
    }
 
-   private def procBody() {
-      import synth._
-      import ugen._
-      val s             = Server.dummy
-      val spec          = AudioFile.readSpec( settings.audioInput )
-      val stepSize      = settings.fftSize / settings.fftOverlap
-      val coeffBufSize  = 1024
-      val fftBufID      = 0
-      val coeffBufID    = 1
-      val df = SynthDef( "nrt" ) {
-         val in         = Mix( In.ar( NumOutputBuses.ir, af.numChannels )) // XXX mono mix could be configurable
-         val chain      = FFT( fftBufID, in, 1.0 / settings.fftOverlap )
-         val coeffs     = MFCC.kr( chain, settings.numCoeffs )
-         val rmax       = RunningMax.kr( coeffs, 0 )
-         val rmin       = RunningMin.kr( coeffs, 0 )
-//         Out.kr( 0, rmax :: rmin :: Nil )
-         val trig       = Impulse.ar( SampleRate.ir / stepSize )
-//         SendReply.ar( trig, coeffs, "/feat" )
-         val phase      = Stepper.ar( trig, min = 0, max = coeffBufSize - 1 )
-         BufWr.kr( coeffs, coeffBufID, phase, loop = 0, interp = 1 )
-      }
-
-//      val df2 = SynthDef( "snap" ) {
-//         val coeffs = Latch.ar( K2A.ar( In.kr( 0, numCoeffs * 2 )), Impulse.ar( 0 ))
-//         DiskOut.ar( 2, coeffs )
-////   Line.ar( dur = ControlDur.ir, doneAction = freeSelf )
-//      }
-
-      val syn        = Synth( s )
-      val fftBuf     = new Buffer( s, fftBufID )
-      val coeffBuf   = new Buffer( s, coeffBufID )
-      val inPath     = settings.audioInput.getAbsolutePath
-      val outPath    = settings.featureOutput.getAbsolutePath
-      val numFrames  = ((spec.numFrames + stepSize - 1) / stepSize) * stepSize
-      val dur        = numFrames / spec.sampleRate
-      val bndls      = OSCBundle.secs( 0.0,
-         fftBuf.allocMsg( settings.fftSize ),
-         coeffBuf.allocMsg( coeffBufSize, numCoeffs ),
-         buf3.writeMsg( outPath, numFrames = 0, leaveOpen = true ),
-         df.recvMsg,
-         df2.recvMsg,
-         syn.newMsg( df.name, s.rootNode )
-      ) :: OSCBundle.secs( dur,
-         syn.freeMsg,
-         syn2.newMsg( df2.name, s.rootNode )
-      ) :: OSCBundle.secs( dur + 32768.0/44100,
-         syn2.freeMsg,
-         buf3.closeMsg
-      ) :: Nil
-
-      val c = de.sciss.osc.OSCPacketCodec.default
-      val sz = bndls.map( _.getEncodedSize( c )).max
-
-      val f = File.createTempFile( "tmp", ".osc" )
-      val raf = new RandomAccessFile( f, "rw" )
-      val bb = java.nio.ByteBuffer.allocate( sz )
-      val fch = raf.getChannel()
-      bndls.foreach { bndl =>
-         bndl.encode( c, bb )
-         bb.flip
-         raf.writeInt( bb.limit )
-         fch.write( bb )
-         bb.clear
-      }
-      raf.close
-
-// run from cmdline:
-      "./scsynth -i 1 -o 1 -N " + f.getAbsolutePath + " " + inPath + " /tmp/killme.aif 44100 AIFF int16"
-
-      val afRes = AudioFile.openRead( outPath )
-      val fb = afRes.frameBuffer( 1 )
-      afRes.readFrames( fb )
-      afRes.close
-      fb.map(_(0))
-   }
-   }
-
-   private object Act extends DaemonActor {
-      start()
+   private object Act extends Actor {
       def act() {
+         ProcT.start()
          var result : Result = null
          loopWhile( result == null ) {
             react {
                case Abort =>
-                  result = Aborted
+                  ProcT.synchronized {
+                     ProcT.aborted = true
+                     if( ProcT.p != null ) ProcT.p.destroy()
+                  }
+               case res @ Aborted =>
+                  result = res
+               case res @ Failure( _ ) =>
+                  result = res
+               case res @ Success =>
+                  result = res
             }
          } andThen { observer( result )}
       }
--- src/main/scala/de/sciss/utopia/Utopia.scala
+++ src/main/scala/de/sciss/utopia/Utopia.scala
@@ -1,7 +1,27 @@
 package de.sciss.utopia
 
+import java.io.File
+
 object Utopia {
    def main( args: Array[ String ]) {
+      import FeatureExtraction._
 
+      val inDir   = "/Users/hhrutz/Desktop/new_projects/Utopia/audio_work"
+      val name    = "Raspad_30'58"
+      val set     = new SettingsBuilder
+      set.audioInput    = new File( inDir, name + ".aif" )
+      set.featureOutput = new File( inDir, name + "_feat.aif" )
+      set.metaOutput    = Some( new File( inDir, name + "_feat.xml" ))
+
+      println( "Starting extraction..." )
+      FeatureExtraction( set ) {
+         case Success =>
+            println( "Success." )
+         case Failure( e ) =>
+            println( "Failed: " )
+            e.printStackTrace()
+         case Progress( f ) =>
+            println( (f * 100).toInt )
+      }
    }
 }
